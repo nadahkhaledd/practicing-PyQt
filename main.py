37fcfeb33814458a7b45c1baf9f6aab62b3231f9
nadahkhaledd <nadahkhaledd@gmail.com>
2022-03-20 20:11:14 +0200

    created basic app to understand

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+#!/usr/bin/pyton3
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import sys
+
+class Page(QWidget):
+    def __init__(self, parent = None):
+        super(Page, self).__init__(parent)
+        label = QLabel('first label')
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(layout, 0, 1)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle('first Qt App')
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    window = Page()
+    window.show()
+    sys.exit(app.exec())
+
+
 
